1852ee4e49b70f1676b8938f96a4df2206999d2a
Anton <be1337@live.ru>
2021-09-01 10:25:54 +0300

    HomeWork1 final

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ console.log(nOf);
 //2
 const personalMovieDB = {
     count: nOf,
-    moveis: {},
+    movies: {},
     actors: {},
     genres: [],
     privat: false
@@ -16,7 +16,6 @@ const personalMovieDB = {
 console.log(personalMovieDB);
 
 //3
-const arr = [];
 const qFilm1 = prompt ('1. What is the last movie?',"Spider-man");
 const filmRate1 = prompt ('1. How much?',"9.5");
 const qFilm2 = prompt ('2. What is the last movie?',"Wonder-women");
@@ -24,18 +23,15 @@ const filmRate2 = prompt ('2. How much?',"8.5");
 
 console.log(qFilm1,filmRate1,qFilm2,filmRate2);
 
-const name1 = qFilm1;
-const name2 = qFilm2;
+// const name1 = qFilm1;
+// const name2 = qFilm2;
+// const movies = {
+// };
 
-const movies = {
-    
-};
-movies[name1] = filmRate1;
-movies[name2] = filmRate2;
-
+personalMovieDB.movies[qFilm1]=filmRate1;
+personalMovieDB.movies[qFilm2]=filmRate2;
 
 
-
-console.log(movies);
+console.log(personalMovieDB);
 
 
