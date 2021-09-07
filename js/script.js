2b457cdd7e311cc647b2aab1491c4befd94f1c47
Anton <be1337@live.ru>
2021-09-07 13:29:00 +0300

    pracc functions

--- js/script.js
+++ js/script.js
@@ -1,37 +1,67 @@
 "use strict";
 
-//1
-const nOf = +prompt ("Ckolko filmov smotreli", "?");
-console.log(nOf);
 
-//2
+let numberOfFilms;
+
+function start(){
+    numberOfFilms = +prompt ("skol'ko smotreli?", "");
+    
+    while (numberOfFilms == "" || numberOfFilms == null || isNaN (numberOfFilms)){
+        numberOfFilms = +prompt ("skol'ko smotreli?", "");
+    }
+}
+
+start();
+
 const personalMovieDB = {
-    count: nOf,
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-console.log(personalMovieDB);
+function showMyDB(hidden){
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
 
-//3
-const qFilm1 = prompt ('1. What is the last movie?',"Spider-man");
-const filmRate1 = prompt ('1. How much?',"9.5");
-const qFilm2 = prompt ('2. What is the last movie?',"Wonder-women");
-const filmRate2 = prompt ('2. How much?',"8.5");
+function writeYourGenres(){
+    let num;
+    for (let i=0;i<3;i++){
+        personalMovieDB.genres[i]=prompt(`Ваш любимый жанр под номером ${i+1}`, "");
+        console.log((i+1), ' eto ', personalMovieDB.genres);
 
-console.log(qFilm1,filmRate1,qFilm2,filmRate2);
+    }
+}
+writeYourGenres();
 
-// const name1 = qFilm1;
-// const name2 = qFilm2;
-// const movies = {
-// };
+// function rememberMyFilms(){
+//     for (let i=0;i<2;i++){
+//         const a = prompt("last movie?", ""),
+//               b = prompt("rate movie", "");
+//         if (a!=null&&b!=null&&a!=""&&b!=""&&a.length<50){
+//             personalMovieDB.movies[a]=b;
+//             console.log('done');
+//         }else{
+//             console.log('error');
+//             i--;
+//         }  
+//     }
+// }
+// rememberMyFilms();
 
-personalMovieDB.movies[qFilm1]=filmRate1;
-personalMovieDB.movies[qFilm2]=filmRate2;
-
-
-console.log(personalMovieDB);
-
-
+// function detectPersonalLevel(){
+//     if (personalMovieDB.count < 10){
+//         console.log("Prosmotreno malo filmov");    
+//     } else if (personalMovieDB.count >=10&&personalMovieDB.count<30){
+//         console.log("Klassicheskiy zritel");  
+//     } else if (personalMovieDB.count>=30){
+//         console.log("Kinoman");
+//     } else {
+//         console.log("ERROR");
+//     }
+// }
+// detectPersonalLevel();
