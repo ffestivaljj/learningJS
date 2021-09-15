63a122f08558a2c152772e3d2f2ab5cc331a5654
Anton <be1337@live.ru>
2021-09-15 23:50:16 +0300

    +pracc

--- js/pracc.js
+++ js/pracc.js
@@ -1,5 +1,102 @@
 "use strict";
 
-const arr = [1, 2, 4, 6, 8];
+let a = 5,
+    b = a;
+b = b + 5;
+console.log(a);
+console.log(b);
 
-arr.pop();
+const obj = {
+    a:55,
+    b:12
+};
+
+// const copy = obj;
+
+// copy.a = 10;
+// console.log(copy);
+// console.log(obj);
+
+function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+
+    return objCopy;
+}
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        a: 7,
+        b: 3
+    }
+};
+
+const newNumbers = copy(numbers);
+newNumbers.a = 111;
+console.log(newNumbers);
+console.log(numbers);
+
+const add = {
+    d:14,
+    e:20
+};
+
+const clone = Object.assign({},add);
+clone.d = 20;
+// console.log(add);
+// console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'asdasd';
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['yt', 'vimeo', 'rutube'],
+        blogs = ['wp', 'lj', 'blogger'],
+        internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+
+// const arr = [1, 80, 2, 4, 6, 8];
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b) {
+//     return a-b;
+// }
+
+// arr.forEach(function(item, i, arr){
+//     console.log(`${i}: ${item} vnutri massiva ${arr}`);
+// });
+
+
+// console.log(arr.length);
+// arr[99] = 123;
+// console.log(arr.length);
+// console.log(arr);
+// arr.pop();
+
+// arr.push(5);
+// console.log(arr);
+
+// for (let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr){
+//     console.log(value);
+// }
+
+// const str = prompt("","");
+// const products = str.split(" ");
+// products.sort();
+// console.log(products.join('; '));
--- js/script.js
+++ js/script.js
@@ -1,67 +1,67 @@
 "use strict";
 
 
-let numberOfFilms;
+// let numberOfFilms;
 
-function start(){
-    numberOfFilms = +prompt ("skol'ko smotreli?", "");
+// function start(){
+//     numberOfFilms = +prompt ("skol'ko smotreli?", "");
     
-    while (numberOfFilms == "" || numberOfFilms == null || isNaN (numberOfFilms)){
-        numberOfFilms = +prompt ("skol'ko smotreli?", "");
-    }
-}
-
-start();
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-function showMyDB(hidden){
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres(){
-    let num;
-    for (let i=0;i<3;i++){
-        personalMovieDB.genres[i]=prompt(`Ваш любимый жанр под номером ${i+1}`, "");
-        console.log((i+1), ' eto ', personalMovieDB.genres);
-
-    }
-}
-writeYourGenres();
-
-// function rememberMyFilms(){
-//     for (let i=0;i<2;i++){
-//         const a = prompt("last movie?", ""),
-//               b = prompt("rate movie", "");
-//         if (a!=null&&b!=null&&a!=""&&b!=""&&a.length<50){
-//             personalMovieDB.movies[a]=b;
-//             console.log('done');
-//         }else{
-//             console.log('error');
-//             i--;
-//         }  
+//     while (numberOfFilms == "" || numberOfFilms == null || isNaN (numberOfFilms)){
+//         numberOfFilms = +prompt ("skol'ko smotreli?", "");
 //     }
 // }
-// rememberMyFilms();
 
-// function detectPersonalLevel(){
-//     if (personalMovieDB.count < 10){
-//         console.log("Prosmotreno malo filmov");    
-//     } else if (personalMovieDB.count >=10&&personalMovieDB.count<30){
-//         console.log("Klassicheskiy zritel");  
-//     } else if (personalMovieDB.count>=30){
-//         console.log("Kinoman");
-//     } else {
-//         console.log("ERROR");
+// start();
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// function showMyDB(hidden){
+//     if (!hidden) {
+//         console.log(personalMovieDB);
 //     }
 // }
-// detectPersonalLevel();
+// showMyDB(personalMovieDB.privat);
+
+// function writeYourGenres(){
+//     let num;
+//     for (let i=0;i<3;i++){
+//         personalMovieDB.genres[i]=prompt(`Ваш любимый жанр под номером ${i+1}`, "");
+//         console.log((i+1), ' eto ', personalMovieDB.genres);
+
+//     }
+// }
+// writeYourGenres();
+
+// // function rememberMyFilms(){
+// //     for (let i=0;i<2;i++){
+// //         const a = prompt("last movie?", ""),
+// //               b = prompt("rate movie", "");
+// //         if (a!=null&&b!=null&&a!=""&&b!=""&&a.length<50){
+// //             personalMovieDB.movies[a]=b;
+// //             console.log('done');
+// //         }else{
+// //             console.log('error');
+// //             i--;
+// //         }  
+// //     }
+// // }
+// // rememberMyFilms();
+
+// // function detectPersonalLevel(){
+// //     if (personalMovieDB.count < 10){
+// //         console.log("Prosmotreno malo filmov");    
+// //     } else if (personalMovieDB.count >=10&&personalMovieDB.count<30){
+// //         console.log("Klassicheskiy zritel");  
+// //     } else if (personalMovieDB.count>=30){
+// //         console.log("Kinoman");
+// //     } else {
+// //         console.log("ERROR");
+// //     }
+// // }
+// // detectPersonalLevel();
